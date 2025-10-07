031f00d963615196aa9a69b9074babffa85b06a7
Evan <evantrujillo30@gmail.com>
2025-10-07 09:39:53 +0900

    fix: import errors

--- domains/invoices/server/repositories/InvoiceRepository.ts
+++ domains/invoices/server/repositories/InvoiceRepository.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
-import type { Invoice } from "~/domains/invoices/types"
+import type { Invoice } from '../../types'
 import prisma from "../../../../lib/prisma"
 
 interface IInvoiceRepository {
