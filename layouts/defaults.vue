23b39708e280125a69020e2795b800ae51dda067
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-09 22:39:43 +0900

    feat: add Invoice layout

--- layouts/defaults.vue
+++ layouts/defaults.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <p>This is the default layout</p>
-    <slot />
+  <div class="flex h-screen bg-background">
+    <aside
+      class="w-64 bg-gray-200 p-4 border-r border-gray-300 flex flex-col justify-between"
+    >
+      <slot name="sidebar" />
+
+      <!-- <div class="h-12 bg-gray-400 rounded"></div>
+
+      <div class="space-y-3">
+        <div class="h-8 bg-gray-300 rounded"></div>
+        <div class="h-8 bg-gray-300 rounded"></div>
+      </div> -->
+    </aside>
+
+    <!-- <main class="flex-1 grid place-items-center p-6">
+      <div class="h-32 w-1/2 bg-gray-200 rounded"></div>
+    </main> -->
   </div>
 </template>
