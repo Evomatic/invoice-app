8fdc0faac96ad74ea070627e20e2cb0a003c9dcf
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-11 21:32:48 +0900

    fix: layout responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="flex h-screen bg-background">
+  <div class="flex flex-col md:flex-row h-screen bg-background">
     <aside
-      class="w-64 bg-gray-200 p-4 border-r border-gray-300 flex flex-col justify-between"
+      class="w-full md:w-26 bg-gray-200 p-4 border-b md:border-b-0 md:border-r border-gray-300 flex flex-row md:flex-col justify-between"
     >
-      <slot name="sidebar" />
-
-      <div class="h-12 bg-gray-400 rounded"></div>
-
-      <div class="space-y-3">
-        <div class="h-8 bg-gray-300 rounded"></div>
-        <div class="h-8 bg-gray-300 rounded"></div>
+      <div class="h-[39px] bg-gray-400 rounded" />
+      <div class="flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
+        <div class="h-8 w-8 md:w-auto bg-gray-300 rounded" />
+        <div class="h-8 w-8 md:w-auto bg-gray-300 rounded" />
       </div>
     </aside>
 
-    <main class="flex-1 grid place-items-center p-6">
+    <main class="flex-1 grid place-items-center p-6 overflow-auto">
       <slot />
     </main>
   </div>
