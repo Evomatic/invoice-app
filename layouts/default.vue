4ef899f8982fbea2d0cf3f90850609742647167a
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-12 23:46:41 +0900

    feat: create header logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="flex flex-col md:flex-row h-screen bg-background">
     <aside
-      class="w-full md:w-26 bg-gray-200 p-4 border-b md:border-b-0 md:border-r border-gray-300 flex flex-row md:flex-col justify-between"
+      class="w-full md:w-26 bg-[#373B53] border-b md:border-b-0 md:border-r flex flex-row md:flex-col"
     >
-      <div class="h-[39px] bg-gray-400 rounded" />
       <div class="flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
-        <div class="h-8 w-8 md:w-auto bg-gray-300 rounded" />
-        <div class="h-8 w-8 md:w-auto bg-gray-300 rounded" />
+        <div class="grid items-center justify-center relative w-20 h-20 bg-primary rounded-r-3xl overflow-hidden">
+          <img src="/svg/logo.svg" alt="The header logo." class="z-10" />
+          <div class="absolute bottom-0 left-0 w-full h-1/2 bg-indigo-400 rounded-tl-3xl" />
+        </div>
       </div>
     </aside>
 
