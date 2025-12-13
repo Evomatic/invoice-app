644501cd02deb3f422fe8489d5db9499897fae9e
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-13 21:05:05 +0900

    fix: complete header responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="flex flex-col md:flex-row h-screen bg-background">
-    <aside
-      class="w-full md:w-26 bg-[#373B53] border-b md:border-b-0 md:border-r flex flex-row md:flex-col"
-    >
+    <aside class="md:rounded-tr-3xl md:rounded-br-3xl w-full md:w-26 bg-[#373B53] border-b md:border-b-0 md:border-r flex flex-row md:flex-col">
       <div class="flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
-        <div class="grid items-center justify-center relative w-20 h-20 bg-primary rounded-r-3xl overflow-hidden">
-          <img src="/svg/logo.svg" alt="The header logo." class="z-10" />
-          <div class="absolute bottom-0 left-0 w-full h-1/2 bg-indigo-400 rounded-tl-3xl" />
+        <div
+          class="
+    relative
+    w-20 h-20
+    md:w-full md:h-26
+    grid items-center justify-center
+    bg-primary
+    rounded-r-3xl md:rounded-none md:rounded-br-3xl md:rounded-tr-3xl
+    overflow-hidden
+  "
+        >
+          <img
+            src="/svg/logo.svg"
+            alt="The header logo."
+            class="z-10 md:h-10 md:w-10"
+          >
+          <div
+            class="
+      absolute bottom-0 left-0 w-full h-1/2
+      bg-indigo-400
+      rounded-tl-2xl
+    "
+          />
         </div>
       </div>
     </aside>
