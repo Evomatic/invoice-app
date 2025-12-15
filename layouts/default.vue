6728a92f9d5e9d054a4c3f11aed38f5b490bce24
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-15 22:00:41 +0900

    feat: add to switcher implementation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col md:flex-row h-screen bg-background">
+  <div class="flex flex-col md:flex-row h-screen bg-background" :data-theme="currentTheme">
     <aside
-      class="flex justify-between md:rounded-tr-3xl md:rounded-br-3xl w-full md:w-26 bg-[#373B53] border-b md:border-b-0 md:border-r lex-row md:flex-col"
+      class="flex justify-between md:rounded-tr-3xl md:rounded-br-3xl w-full md:w-26 bg-navy-600 border-b md:border-b-0 md:border-r lex-row md:flex-col"
     >
       <div class="flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
         <div
@@ -29,12 +29,13 @@
           />
         </div>
       </div>
-       <div class="relative md:w-full w-20 h-20 md:h-22 ml-auto md:ml-0 md:mt-auto">
-        <img
-          class="h-5 w-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          src="/svg/icon-moon.svg"
-          alt="The header moon theme button icon."
-        >
+      <div class="flex flex-col justify-center md:w-full w-20 h-20 md:h-22 ml-auto md:ml-0 md:mt-auto">
+        <Button class="bg-navy-600 hover:bg-navy-600">
+          <img
+            src="/svg/icon-moon.svg"
+            alt="The header moon theme button icon."
+          >
+        </Button>
       </div>
       <div class="relative md:w-full w-20 h-20 md:h-22 border-l-2 md:border-t-1 border-gray-600 md:border-l-0">
         <img
@@ -50,3 +51,12 @@
     </main>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
+
+const currentTheme = ref<'light' | 'dark'>('light' as const)
+
+
+</script>
