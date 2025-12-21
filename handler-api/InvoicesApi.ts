d30bbc54ebad52a836d91f1ac4095ff4023fb7d4
Evan Trujillo <evantrujillo30@gmail.com>
2025-12-21 21:53:24 +0900

    feat: use useFetch over fetch library [EVO-19]

--- handler-api/InvoicesApi.ts
+++ handler-api/InvoicesApi.ts
@@ -1,32 +1,25 @@
-import { $fetch } from 'ofetch'
 import type { Invoice } from '../types'
+import { useFetch } from 'nuxt/app'
 
-interface IInvoiceApi {
-  listInvoices(): Promise<Invoice[]>
-  getInvoiceById(id: string): Promise<Invoice>
-  createInvoice(data: Partial<Invoice>): Promise<Invoice>
-  updateInvoice(id: string, data: Partial<Invoice>): Promise<Invoice>
-  deleteInvoice(id: string): Promise<void>
-}
 
-export default class InvoiceApi implements IInvoiceApi {
-  async listInvoices() {
-    return $fetch<Invoice[]>('api/v1/invoices')
+export default class InvoiceApi {
+  listInvoices() {
+    return useFetch<Invoice[]>('api/v1/invoices')
   }
 
   async getInvoiceById(id: string) {
-    return $fetch<Invoice>(`api/v1/invoices/${id}`)
+    return useFetch<Invoice>(`api/v1/invoices/${id}`)
   }
 
   async createInvoice(data: Partial<Invoice>) {
-    return $fetch<Invoice>('api/v1/invoices', { method: 'POST', body: data })
+    return useFetch<Invoice>('api/v1/invoices', { method: 'POST', body: data })
   }
 
   async updateInvoice(id: string, data: Partial<Invoice>) {
-    return $fetch<Invoice>(`api/v1/invoices/${id}`, { method: 'PATCH', body: data })
+    return useFetch<Invoice>(`api/v1/invoices/${id}`, { method: 'PATCH', body: data })
   }
 
   async deleteInvoice(id: string) {
-    return $fetch(`api/v1/invoices/${id}`, { method: 'DELETE' })
+    return useFetch(`api/v1/invoices/${id}`, { method: 'DELETE' })
   }
 }
