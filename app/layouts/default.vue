380f65e04c046ad04587e4367d51f3054c21f590
evomatic <evantrujillo30@gmail.com>
2026-01-01 16:27:51 +0900

    fix: responsiveness of default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,27 +1,27 @@
 <template>
   <div
-    class="flex flex-col md:flex-row h-screen bg-background dark:bg-navy-900"
+    class="flex flex-col lg:flex-row h-screen bg-background dark:bg-navy-900"
     :data-theme="currentTheme"
   >
     <aside
-      class="border-none flex justify-between md:rounded-tr-3xl md:rounded-br-3xl w-full md:w-26 bg-navy-600 border-b md:border-b-0 md:border-r lex-row md:flex-col"
+      class="border-none flex justify-between lg:rounded-tr-3xl lg:rounded-br-3xl w-full lg:w-26 bg-navy-600 border-b lg:border-b-0 flex-row lg:flex-col"
     >
-      <div class="flex flex-row md:flex-col space-x-3 md:space-x-0 md:space-y-3">
+      <div class="flex flex-row lg:flex-col space-x-3 lg:space-x-0 lg:space-y-3">
         <div
           class="
-    relative
-    w-20 h-20
-    md:w-full md:h-26
-    grid items-center justify-center
-    bg-primary
-    rounded-r-3xl md:rounded-none md:rounded-br-3xl md:rounded-tr-3xl
-    overflow-hidden
-  "
+          relative
+          w-20 h-20
+          lg:w-full lg:h-26
+          grid items-center justify-center
+          bg-primary
+          rounded-r-3xl lg:rounded-none lg:rounded-br-3xl lg:rounded-tr-3xl
+          overflow-hidden
+        "
         >
           <img
             src="/svg/logo.svg"
             alt="The header logo."
-            class="z-10 md:h-10 md:w-10"
+            class="z-10 lg:h-10 lg:w-10"
           >
           <div
             class="
@@ -32,7 +32,7 @@
           />
         </div>
       </div>
-      <div class="flex flex-col justify-center md:w-full w-20 h-20 md:h-22 ml-auto md:ml-0 md:mt-auto">
+      <div class="flex flex-col justify-center lg:w-full w-20 h-20 lg:h-22 ml-auto lg:ml-0 lg:mt-auto">
         <Button
           class="bg-navy-600 hover:bg-navy-600"
           @click="toggleTheme"
@@ -43,9 +43,9 @@
           >
         </Button>
       </div>
-      <div class="relative md:w-full w-20 h-20 md:h-22 border-l-2 md:border-t-1 border-gray-600 md:border-l-0">
+      <div class="relative lg:w-full w-20 h-20 lg:h-22 border-l-2 lg:border-t-1 border-gray-600 lg:border-l-0">
         <img
-          class=" md:w-10 md:h-10 rounded-full h-8 w-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          class=" lg:w-10 lg:h-10 rounded-full h-8 w-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           src="/image-avatar.jpg"
           alt="The header avatar image."
         >
